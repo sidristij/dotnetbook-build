eaeec09a877d8fcc54919f63c387d918885a58c6
Stanislav Sidristij <sunex.development@gmail.com>
2020-01-21 23:49:19 +0300

    basic structure processing

--- Pipeline/Common/Structure/DocumentStructureEntry.cs
+++ Pipeline/Common/Structure/DocumentStructureEntry.cs
@@ -9,13 +9,21 @@ namespace BookBuilder.Pipeline.Common.Structure
     {
         private ConcurrentBag<DocumentStructureEntry> _subentries;
         private string title;
+        private int depth;
 
-        public DocumentStructureEntry(FileDescription source, MarkdownDocument document, HeadingBlock block, DocumentStructureEntry parent = null)
+        public DocumentStructureEntry(FileDescription source, MarkdownDocument document, HeadingBlock block, int depth = 0, DocumentStructureEntry parent = null)
         {
             Source = source;
             Document = document;
             Block = block;
+            this.depth = depth;
             Parent = parent;
+            _subentries = new ConcurrentBag<DocumentStructureEntry>();
+        }
+
+        public void AddChild(DocumentStructureEntry entry)
+        {
+            _subentries.Add(entry);
         }
 
         public DocumentStructureEntry WithTitle(string title)
@@ -24,7 +32,15 @@ namespace BookBuilder.Pipeline.Common.Structure
             return this;
         }
 
+        public DocumentStructureEntry WithDepth(int depth)
+        {
+            this.depth = depth;
+            return this;
+        }
+
         public FileDescription Source { get; }
+
+        public int Depth => depth;
 
         public ReadOnlySpan<char> Title => Block?.ToPositionText() ?? title;
 
--- Pipeline/MarkdownParsingProcessor.cs
+++ Pipeline/MarkdownParsingProcessor.cs
@@ -4,6 +4,7 @@ using BookBuilder.Pipeline.Common;
 using BookBuilder.Pipeline.Common.Structure;
 using Markdig;
 using Markdig.Parsers;
+using Markdig.Syntax;
 
 namespace BookBuilder.Pipeline
 {
@@ -28,7 +29,42 @@ namespace BookBuilder.Pipeline
                 .With(document)
                 .With(documentStructure);
 
+            VisitDocument(document, documentStructure);
+            
             ProjectProcessing.TryAddTask(new MarkdownDocumentProcessor(ctx));
+        }
+
+        private void VisitDocument(MarkdownDocument document, DocumentStructureEntry entry)
+        {
+            foreach (var markdownObject in document.Descendants())
+            {
+                VisitMarkdownObject(document, markdownObject, ref entry);    
+            }
+        }
+
+        private void VisitMarkdownObject(MarkdownDocument document, MarkdownObject markdownObject, ref DocumentStructureEntry entry)
+        {
+            if (markdownObject is HeadingBlock header)
+            {
+                if (entry.Depth > header.Level - 1)
+                {
+                    var parent = entry;
+                    entry = new DocumentStructureEntry(FileDescription, document, header, header.Level, entry);
+                    parent.AddChild(entry);
+                } else if (entry.Depth <= header.Level)
+                {
+                    var cur = entry;
+                    while (cur.Depth > header.Level) cur = cur.Parent;
+                    if (cur.Depth >= header.Level) cur = cur.Parent;
+                    entry = new DocumentStructureEntry(FileDescription, document, header, header.Level, cur);
+                    cur.AddChild(entry);
+                } 
+            }
+
+            foreach (var descendant in markdownObject.Descendants())
+            {
+                VisitMarkdownObject(document, descendant, ref entry);
+            }
         }
     }
 }
