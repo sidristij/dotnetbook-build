c6d73e81f1d6031177ad83172fa6078ce9932735
Stanislav Sidristij <sunex.development@gmail.com>
2020-03-24 13:35:30 +0300

    hard version of para`

--- Pipeline/MarkdownDocumentProcessor.cs
+++ Pipeline/MarkdownDocumentProcessor.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using BookBuilder.Pipeline.Common;
 using Markdig;
@@ -34,6 +35,11 @@ namespace BookBuilder.Pipeline
             await renderer.Writer.FlushAsync();
 
             var result = writer.ToString();
+
+            result = result
+                .Replace(" - ", " &mdash; ")
+                .Replace("\" ", "&raquo; ", StringComparison.Ordinal)
+                .Replace(" \"", " &laquo;", StringComparison.Ordinal);
 
             var ctx = Context.CreateCopy().With(new DocumentHolder{ DocumentBody = result });
             
