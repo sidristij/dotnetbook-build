925b0f47b51bae83486574c724d45aa88e00dea9
Stanislav Sidristij <sunex.development@gmail.com>
2020-04-11 15:36:12 +0300

    link fix

--- Extensions/Sidenotes/HtmlSidenoteLinkRenderer.cs
+++ Extensions/Sidenotes/HtmlSidenoteLinkRenderer.cs
@@ -20,7 +20,7 @@ namespace BookBuilder.Extensions.Footnotes
 
         protected override void Write(HtmlRenderer renderer, SidenoteLink link)
         {
-            var order = link.Sidenote.Label;
+            var order = link.Sidenote.Label.Substring(1).Trim();
             renderer.Write(link.IsBackLink
                 ? $"<a href=\"#fnref:{link.Index}\" class=\"{FootnoteBackLinkClass}\">{BackLinkString}</a>"
                 : $"<a id=\"fnref:{link.Index}\" href=\"#fn:{order}\" class=\"{FootnoteLinkClass}\"><sup>{order}</sup></a>");
